e0f4f99c7811b9befad05d4a9d509af17f6e51a0
binoytv9 <binoytv9@gmail.com>
2015-02-06 16:58:52 +0530

    & > |

--- shell.c
+++ shell.c
@@ -9,9 +9,9 @@
 extern char **environ;
 
 int getch(void);
-void ungetch(int c);
-void stripToArg(char *line, char *ap[]);
-int getLine(char *line, int *toBackground, int *redirection, char *redirectionFile, int ml);
+void ungetch(int);
+void stripToArg(char *, char **);
+int getLine(char *, char **, int *, int *, int *, char *, int);
 
 char buffer[MAX_LENGTH];
 char *bufp = buffer;
@@ -20,30 +20,53 @@ int main(int argc, char *argv[])
 {
 	int fd;
 	pid_t childPid;
+	int redirection;
+	int pipeEnable;
+	int toBackground;
+	char *line2 = NULL;
 	char *argVec[MAX_ARG];
+	char *argVec2[MAX_ARG];
 	char line[MAX_LENGTH];
 	char redirectionFile[MAX_LENGTH];
-	int toBackground;
-	int redirection;
 
 	if(argc > 1 || (argc > 1 && strcmp(argv[1],"--help") == 0))
 		usageErr("%s - a simple SHELL interpreter\nNo arguments required\n",argv[0]);
 
-	toBackground = false;
+	int pfd[2];
+	if(pipe(pfd) == -1)
+		errExit("pipe");
+
+	pipeEnable = false;
 	redirection = false;
-	while(getLine(line, &toBackground, &redirection, redirectionFile, MAX_LENGTH) != EOF){
+	toBackground = false;
+	while(getLine(line, &line2, &pipeEnable, &toBackground, &redirection, redirectionFile, MAX_LENGTH) != EOF){
 		stripToArg(line, argVec);
+		stripToArg(line2, argVec2);
 		if(argVec[0] != NULL){
 			switch(childPid = fork()){
-				case -1:/* error */
+				/* error */
+				case -1:
 					errExit("fork");
-
-				case 0 :/* child  */
-					if(redirection){
-						fd = open(redirectionFile, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-						close(1);
-						dup(fd);
+				/* child  */
+				case 0 :
+					if(pipeEnable){
+						if(close(pfd[0]) == -1)
+							errExit("close 1");
+						if(pfd[1] != STDOUT_FILENO){
+							if(dup2(pfd[1], STDOUT_FILENO) == -1)
+								errExit("dup2 1");
+							if(close(pfd[1]) == -1)
+								errExit("close 2");
+						}
 					}
+
+					if(redirection){
+						fd = open(redirectionFile, O_WRONLY | O_TRUNC | O_CREAT, 
+								S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+						close(STDOUT_FILENO);	/* closing stdout */
+						dup(fd);		/* fd and 1 points to same file */
+					}
+
 					execvp(argVec[0], argVec);
 					if(errno == ENOENT){ /* file not found */
 						fprintf(stderr,"%s: command not found\n",argVec[0]);
@@ -51,15 +74,58 @@ int main(int argc, char *argv[])
 					}
 					else
 						errExit("execve");
-
-				default:/* parent */
-					if(!toBackground && wait(NULL) == -1)
+				/* parent */
+				default:
+					if(!pipeEnable && !toBackground && wait(NULL) == -1)
 						errExit("wait");
+					/* resetting flags */
 					redirection = false;
 					toBackground = false;
 					break;
 			}
 		}
+
+		if(pipeEnable){
+			switch(fork()){
+				/* error */
+				case -1:
+					errExit("fork");
+				/* child  */
+				case 0 :
+					if(pipeEnable){
+						if(close(pfd[1]) == -1)
+							errExit("close 3");
+						if(pfd[0] != STDIN_FILENO){
+							if(dup2(pfd[0], STDIN_FILENO) == -1)
+								errExit("dup2 2");
+							if(close(pfd[0]) == -1)
+								errExit("close 4");
+						}
+					}
+					execvp(argVec2[0], argVec2);
+					if(errno == ENOENT){ /* file not found */
+						fprintf(stderr,"%s: command not found\n",argVec[0]);
+						exit(EXIT_FAILURE);
+					}
+					else
+						errExit("execve");
+				/* parent */
+				default:
+					break;
+			}
+
+			if (close(pfd[0]) == -1)
+				errExit("close 5");
+			if (close(pfd[1]) == -1)
+				errExit("close 6");
+			if (wait(NULL) == -1)
+				errExit("wait 1");
+			if (wait(NULL) == -1)
+				errExit("wait 2");
+
+			pipeEnable = false;
+		}
+
 	}
 	putchar('\n');
 
@@ -68,15 +134,16 @@ int main(int argc, char *argv[])
 
 
 /* takes a line and returns its length on success or EOF if error */
-int getLine(char *line, int *toBackground, int *redirection, char *redirectionFile, int ml)
+int getLine(char *line, char **line2, int *pipeEnable, int *toBackground, int *redirection, char *redirectionFile, int ml)
 {
 	int c;
-	int len;
+	char *lp;
+	int len, len2;
 
 	printf(">>> ");
 
 	len = 0;
-	while(len < ml && (c = getch()) != '\n' && c != '&' && c != '>' && c != EOF){
+	while(len < ml && (c = getch()) != '\n' && c != '&' && c != '>' && c != '|' && c != EOF){
 		*line++ = c;
 		len++;
 	}
@@ -88,13 +155,30 @@ int getLine(char *line, int *toBackground, int *redirection, char *redirectionFi
 	if(c == '>'){
 		*redirection = true;
 
-		while(isspace(c = getch())); // skipping white space between '>' and filename, if any
+		while(isspace(c = getch())); /* skipping white space between '>' and filename, if any */
 		if(!isspace(c))
 			ungetch(c);
 
 		while(!isspace(c = getch()))
 			*redirectionFile++ = c;
 		*redirectionFile = '\0';
+	}
+	if(c == '|'){
+		len2 = 0;
+		if((*line2 = (char *)malloc(sizeof(ml))) == NULL)
+			errExit("malloc");
+		lp = *line2;
+		while(len2 < ml && (c = getch()) != '\n' && c != EOF){
+			*lp++ = c;
+			len2++;
+		}
+		*lp = '\0';
+		if(len2 == 0 && c == EOF){
+			fprintf(stderr, "no program provided after |\n");
+			free(line2);
+			exit(EXIT_FAILURE);
+		}
+		*pipeEnable = true;
 	}
 
 	if(c != '\n' && c != '&')
@@ -116,6 +200,9 @@ void ungetch(int c)
 /* extract each argument and insert it into an array */
 void stripToArg(char *line, char *ap[])
 {
+	if(line == NULL)
+		return;
+
 	char *wp,*lp;
 	char word[MAX_LENGTH];
 
